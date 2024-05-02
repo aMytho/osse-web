ad85556938f0b7189743b7c3191d98d38d5b4782
Jonathan Shull <58316242+aMytho@users.noreply.github.com>
2024-05-02 21:55:08 +0000

    Create project with dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,ts}",
+    ],
+    theme: {
+        extend: {
+            dropShadow: {
+                glow: [
+                    "0 0px 10px rgba(255,255, 255, 0.35)",
+                    "0 0px 35px rgba(255, 255,255, 0.2)"
+                ]
+            }
+        },
+    },
+    plugins: [],
+}
