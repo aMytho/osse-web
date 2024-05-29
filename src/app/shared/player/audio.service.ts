e2bcdd0e7c126a84ce7f2da6418b6ab3bdda48d8
aMytho <58316242+aMytho@users.noreply.github.com>
2024-05-29 18:28:03 -0500

    Yet another buffer attempt

--- src/app/shared/player/audio.service.ts
+++ src/app/shared/player/audio.service.ts
@@ -10,110 +10,83 @@ export class AudioService {
 
   private audioContext = new AudioContext();
   // Initialize audio buffer sources and gain nodes
-  private sourceNode1 = this.audioContext.createBufferSource();
-  private sourceNode2 = this.audioContext.createBufferSource();
   private gainNode = this.audioContext.createGain();
 
   // Create empty buffers to hold audio data
-  private currentBuffer!: AudioBuffer;
-  private nextBuffer!: AudioBuffer;
+  private audio1 = document.createElement('audio') as HTMLAudioElement;
+  private audio2 = document.createElement('audio') as HTMLAudioElement;
+  private audio3 = document.createElement('audio') as HTMLAudioElement;
+  private audio4 = document.createElement('audio') as HTMLAudioElement;
+  private source1 = this.audioContext.createMediaElementSource(this.audio1);
+  private source2 = this.audioContext.createMediaElementSource(this.audio2);
+  private source3 = this.audioContext.createMediaElementSource(this.audio3);
+  private source4 = this.audioContext.createMediaElementSource(this.audio4);
+  private bufferSource = this.audioContext.createBufferSource();
 
   constructor(
-    private apiService: ApiService,
     private bufferService: BufferService
   ) {
-    this.sourceNode1.connect(this.gainNode);
-    this.sourceNode2.connect(this.gainNode);
+    this.source1.connect(this.gainNode);
+    this.source2.connect(this.gainNode);
+    this.source3.connect(this.gainNode);
+    this.source4.connect(this.gainNode);
     this.gainNode.connect(this.audioContext.destination);
-
-    // Event listener to detect end of audio buffer
-    this.sourceNode1.onended = () => {
-      console.log('Buffer 1 finished.', this.audioContext.currentTime);
-      // Get buffer 3
-      this.buferRequest.emit(1);
-    };
-
-    this.sourceNode2.onended = () => {
-      console.log("2 ended");
-      // this.loadFirstBuffer();
-    };
   }
 
   public play() {
-    const time = this.audioContext.currentTime;
-    const duration = this.currentBuffer.duration;
-    this.sourceNode1.start(0);
-    this.sourceNode2.start(time + duration, duration);
+    this.audio1.play();
+    this.buferRequest.emit(2);
   }
 
   public async loadFirstBuffer() {
-    let decoded = await this.audioContext.decodeAudioData(this.bufferService.getEndBuffer().data.slice(0));
-    this.currentBuffer = decoded;
-    this.sourceNode1.buffer = this.currentBuffer;
+    let blob = new Blob([this.bufferService.getEndBuffer().data.slice(0)], { type: 'audio/mpeg' });
+    this.audio1.src = URL.createObjectURL(blob);
     console.log("Buffer 1 loaded");
-
-    // Request buffer 2 data
-    this.buferRequest.emit(2);
   }
 
   public async loadBuffer2() {
     // Fetch and decode the next audio buffer asynchronously
     let combinedBuffer = this.bufferService.getAllBufferData();
-
     // Now `combinedBuffer` contains the combined data of buffer1 and buffer2
 
-    let decoded = await this.audioContext.decodeAudioData(combinedBuffer);
-    this.nextBuffer = decoded;
-    // Swap current and next buffers
+    let blob = new Blob([combinedBuffer], { type: 'audio/mpeg' });
+    this.audio2.src = URL.createObjectURL(blob);
+    this.audio2.load();
+    let tm = 0;
+    
+    this.audio1.addEventListener('ended', () => {
+      // Calculate duration when audio1 ends
+      let duration = this.audio1.currentTime;
+      // Set currentTime of audio2
+      this.audio2.currentTime = tm;
 
-    // Schedule the next buffer to start playing immediately after the current buffer ends
-    this.sourceNode2.buffer = this.nextBuffer;
-    console.log("Source 2 buffer duration:", this.nextBuffer);
+      // Play audio2
+      this.audio2.play();
+
+      // Optionally, update buffer1
+      this.updateBuffer1();
+    });
+
+
+
+    this.audio1.addEventListener('timeupdate', (e) => {
+      // Calculate duration when audio1 ends
+      if (this.audio1.currentTime != this.audio1.duration) {
+        tm = this.audio1.currentTime;
+        console.log(tm);
+      }
+    });
+
     console.log("Buffer 2 loaded");
-    console.log(this.bufferService);
   }
 
-
-
-  public async loadNextAudioBuffer() {
-    // Fetch and decode the next audio buffer asynchronously
-    let combinedBuffer = this.bufferService.getAllBufferData();
-
-    // Now `combinedBuffer` contains the combined data of buffer1 and buffer2
-
-    let decoded = await this.audioContext.decodeAudioData(combinedBuffer);
-    this.nextBuffer = decoded;
-    // Swap current and next buffers
-
-    // Schedule the next buffer to start playing immediately after the current buffer ends
-    this.sourceNode2.buffer = this.nextBuffer;
-  }
-
-  public switchToNextBuffer() {
-    // Switch to the preloaded audio buffer
-    if (this.nextBuffer) {
-
-      // this.sourceNode2.start(0, this.currentBuffer.duration);
-      // Load the next audio buffer for seamless transition
-      // this.loadNextAudioBuffer();
-    }
-  }
 
   public async updateBuffer1() {
-    let decoded = await this.audioContext.decodeAudioData(this.bufferService.getAllBufferData());
-    this.currentBuffer = decoded;
+    let data = this.bufferService.getAllBufferData();
+    let blob = new Blob([data], { type: 'audio/mpeg' });
+    // this.audio3.src = URL.createObjectURL(blob);
+    // this.audio2.addEventListener('ended', (e) => this.audio3.play());
 
-    let newSourceNode1 = this.audioContext.createBufferSource();
-    newSourceNode1.connect(this.gainNode);
-    newSourceNode1.buffer = this.currentBuffer;
-
-    console.log("Buffer 1 reloaded");
-
-    this.sourceNode1 = newSourceNode1;
-
-    // Queue playback
-    let duration = this.nextBuffer.duration;
-    this.sourceNode1.start(this.audioContext.currentTime + duration, duration);
     console.log("Buffer 2 queued");
   }
 }
