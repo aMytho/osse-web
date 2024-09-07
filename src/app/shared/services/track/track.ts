64aac14152df081541ebdc89f5fb815f37abf804
aMytho <58316242+aMytho@users.noreply.github.com>
2024-09-07 12:04:27 -0500

    Use inferred album artist if no album artist

--- src/app/shared/services/track/track.ts
+++ src/app/shared/services/track/track.ts
@@ -4,55 +4,55 @@ import { Artist } from "../artist/artist";
 import { OsseTrack } from "./osse-track";
 
 export class Track {
-    public track!: OsseTrack;
-    private trackArtist!: Artist;
-    public bufferSize: number = 0;
+  public track!: OsseTrack;
+  private trackArtist!: Artist;
+  public bufferSize: number = 0;
 
-    constructor(track: OsseTrack, private apiService: ApiService) {
-        this.track = track;
+  constructor(track: OsseTrack, private apiService: ApiService) {
+    this.track = track;
 
-        // Grab the artist info
-        this.getArtist();
+    // Grab the artist info
+    this.getArtist();
+  }
+
+  public get id() {
+    return this.track.id;
+  }
+
+  public get title() {
+    return this.track.title;
+  }
+
+  public get size() {
+    return this.track.size;
+  }
+
+  public get duration() {
+    return this.track.duration;
+  }
+
+  public get durationFormatted() {
+    return getDuration(this.track.duration);
+  }
+
+  public hasArtist(): boolean {
+    return this.track.artist_id != null;
+  }
+
+  public async getArtist() {
+    if (!this.hasArtist) return;
+
+    let artist = await this.apiService.getArtist(this.track.artist_id as number);
+    if (artist) {
+      this.trackArtist = artist;
     }
+  }
 
-    public get id() {
-        return this.track.id;
-    }
+  public get artist() {
+    return this.trackArtist || null;
+  }
 
-    public get title() {
-        return this.track.title;
-    }
-
-    public get size() {
-        return this.track.size;
-    }
-
-    public get duration() {
-        return this.track.duration;
-    }
-
-    public get durationFormatted() {
-        return getDuration(this.track.duration);
-    }
-
-    public hasArtist(): boolean {
-        return this.track.artist_id != null;
-    }
-
-    private async getArtist() {
-        if (!this.hasArtist) return;
-
-        let artist = await this.apiService.getArtist(this.track.artist_id as number);
-        if (artist) {
-            this.trackArtist = artist;
-        }
-    }
-
-    public get artist() {
-        return this.trackArtist || null;
-    }
-
-    public get trackNumber() {
-      return this.track.track_number;
-    }
+  public get trackNumber() {
+    return this.track.track_number;
+  }
 }
