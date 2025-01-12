484ee8d3e08b417c1d03995aed8c183a04e91bc6
aMytho <58316242+aMytho@users.noreply.github.com>
2025-01-12 14:24:32 -0600

    Add media session API support

---
+++ src/app/shared/player/media-session.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { PlayerService } from './player.service';
+import { TrackService } from '../services/track/track.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaSessionService {
+  constructor(
+    private playerService: PlayerService,
+    private trackService: TrackService
+  ) {
+    if ("mediaSession" in window.navigator) {
+      try {
+        this.listenForMediaEvents();
+      } catch (error) { }
+      try {
+        this.listenForTrackUpdate();
+      } catch (error) { }
+    }
+  }
+
+  private listenForMediaEvents() {
+    navigator.mediaSession.setActionHandler("play", () => {
+      this.playerService.play();
+    });
+    navigator.mediaSession.setActionHandler("pause", () => {
+      this.playerService.pause();
+    });
+    navigator.mediaSession.setActionHandler("stop", () => {
+      this.playerService.pause();
+    });
+    navigator.mediaSession.setActionHandler("previoustrack", () => {
+      this.trackService.moveToLastTrack();
+    });
+    navigator.mediaSession.setActionHandler("nexttrack", () => {
+      this.trackService.moveToNextTrack();
+    });
+  }
+
+  private listenForTrackUpdate() {
+    this.playerService.trackUpdated.subscribe((t) => {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: t.title,
+        artist: t.artist?.name ?? 'Unknown Artist',
+        artwork: [
+          {
+            src: t.cover
+          }
+        ]
+      })
+    })
+  }
+}
