a935ad7ee121e0c146bcea7bdea6a928583fa50c
aMytho <58316242+aMytho@users.noreply.github.com>
2025-01-20 21:42:55 -0600

    Make menu responsive, fix other responsive issues

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IconComponent } from '../shared/ui/icon/icon.component';
-import { mdiCog } from '@mdi/js';
+import { mdiCog, mdiFolderHome, mdiHome, mdiMenu, mdiMenuClose } from '@mdi/js';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
-    selector: 'app-navigation',
-    imports: [RouterLink, RouterLinkActive, IconComponent],
-    templateUrl: './navigation.component.html',
-    styles: ``
+  selector: 'app-navigation',
+  imports: [RouterLink, RouterLinkActive, IconComponent, CommonModule],
+  templateUrl: './navigation.component.html',
+  styles: ``
 })
 export class NavigationComponent {
   gear = mdiCog;
+  home = mdiHome;
+  mobileMenuOpen: WritableSignal<boolean> = signal(false);
+  mobileMenuIcon = computed(() => this.mobileMenuOpen() ? mdiMenuClose : mdiMenu);
+
+  public toggleMenu() {
+    this.mobileMenuOpen.update((v) => !v);
+  }
 }
