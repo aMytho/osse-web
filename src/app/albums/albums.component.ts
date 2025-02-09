e429f293ab8a0f446edffcebed76159b5668127c
aMytho <58316242+aMytho@users.noreply.github.com>
2025-02-09 15:38:47 -0600

    Add album search functionality

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -5,22 +5,36 @@ import { Album } from '../shared/services/album/Album';
 import { HeaderComponent } from '../shared/ui/header/header.component';
 import { OsseAlbum } from '../shared/services/album/osse-album';
 import { fetcher } from '../shared/util/fetcher';
+import { IconComponent } from '../shared/ui/icon/icon.component';
+import { mdiSearchWeb } from '@mdi/js';
 
 @Component({
   selector: 'app-albums',
-  imports: [RouterLink, HeaderComponent],
+  imports: [RouterLink, HeaderComponent, IconComponent],
   templateUrl: './albums.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AlbumsComponent implements OnInit {
   albums: WritableSignal<Album[]> = signal([]);
+  filteredAlbums: WritableSignal<Album[]> = signal([]);
   coverUrlBase: WritableSignal<string> = signal(this.configService.get('apiURL') + "api/tracks/ID/cover");
   loading: WritableSignal<boolean> = signal(true);
+
+  search = mdiSearchWeb;
 
   constructor(
     private configService: ConfigService
   ) { }
+
+  public filterAlbums(event: any) {
+    if (event.target.value.trim().length == 0) {
+      this.filteredAlbums.set(this.albums());
+    } else {
+      const regex = new RegExp(event.target.value.trim(), 'i');
+      this.filteredAlbums.set(this.albums().filter((a) => regex.test(a.name)));
+    }
+  }
 
   async ngOnInit(): Promise<void> {
     let request = await fetcher('albums?tracks=true');
@@ -28,6 +42,7 @@ export class AlbumsComponent implements OnInit {
 
     result.sort((a, b) => a.name.localeCompare(b.name));
     this.albums.set(result.map((a) => new Album(a)));
+    this.filteredAlbums.set(this.albums());
     this.loading.set(false);
   }
 }
